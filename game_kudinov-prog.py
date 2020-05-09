16e04b1d2c28dc2ac96e9246ee171dc9e4ac7654
Kudinov <kudinov.mos@gmail.com>
2020-05-09 17:25:16 +0300

    Create metod hero_equip

--- game_kudinov-prog.py
+++ game_kudinov-prog.py
@@ -58,14 +58,22 @@ def main():
             super().__init__(name, hp_amount, base_attack, base_protection)
             self.hp_amount = hp_amount*2
             self.base_protection = round(base_protection*2, 2)
+            self.final_protection = round(base_protection*2, 2)
             print (f'Cоздан новый персонаж - Паладин {self.name} '
-                   f'с характеристиками: здоровье = {hp_amount},'
-                   f'атака = {base_attack},'
+                   f'с характеристиками: здоровье = {hp_amount}, '
+                   f'атака = {base_attack}, '
                    f'защита = {base_protection}')
 
 
     class Warrior(Person):
-        pass
+        def __init__(self, name, hp_amount, base_attack, base_protection):
+            super().__init__(name, hp_amount, base_attack, base_protection)
+            self.base_attack = base_attack*2
+            self.attack_damage = base_attack*2
+            print (f'Cоздан новый персонаж - Воин {self.name} '
+                   f'с характеристиками: здоровье = {hp_amount}, '
+                   f'атака = {base_attack}, '
+                   f'защита = {base_protection}')
 
 
     def create_things():
@@ -74,21 +82,33 @@ def main():
                         attack=randint(5, 15), life_amount=randint(1, 20))
             things.append(thing)      
 
-    def create_heroes():
-        for new_hero in range(1, 10):
+    def create_heroes_and_equip():
+        len_names = len(NAMES)-1
+        for new_hero in range(0, 10):
             hero_class = CLASSES[randint(0, 1)]
             if hero_class == "Paladin":
-                pass
+                hero = Paladin(name=NAMES[randint(0, len_names)],
+                               hp_amount=100,
+                               base_attack=randint(10, 50),
+                               base_protection=round(0.01*randint(0, 70), 2))
             else:
-                pass
-
-
-    def equip_heroes():
-        pass
-
+                hero = Warrior(name=NAMES[randint(0, len_names)],
+                               hp_amount=100,
+                               base_attack=randint(10, 50),
+                               base_protection=round(0.01*randint(0, 70), 2))
+            hero_things = []
+            for item in range(1, randint(1, 4)):
+                thing = things[randint(1, 24)]
+                hero_things.append(thing)
+            hero.setThings(hero_things)
+            heroes.append(hero)
+        print(f'Итого создано {len(heroes)} героев!')
     def fight():
         pass
+
     create_things()
+    create_heroes_and_equip()
+    #print(heroes[0].hp_amount)
 
 if __name__ == "__main__":
     main()
